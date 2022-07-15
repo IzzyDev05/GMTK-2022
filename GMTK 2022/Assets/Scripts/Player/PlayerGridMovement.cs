badb3c418bd4f5bac75428f0fb60ab1fb36acde8
IzzyDev <66153582+IzzyDev05@users.noreply.github.com>
2022-07-16 02:13:19 +0530

    Player movement randomized

--- GMTK 2022/Assets/Scripts/Player/PlayerGridMovement.cs
+++ GMTK 2022/Assets/Scripts/Player/PlayerGridMovement.cs
@@ -2,22 +2,38 @@ using UnityEngine;
 
 public class PlayerGridMovement : MonoBehaviour
 {
+    [SerializeField] private int translateValue = 1;
+
     private void Update() {
         GridMovement();
     }
 
     private void GridMovement() {
         if (Input.GetKeyDown(KeyCode.W)) {
-            transform.Translate(new Vector3(0, 1, 0));
+            transform.Translate(new Vector3(0, ChooseRandomTranslate(), 0));
         }
         else if (Input.GetKeyDown(KeyCode.S)) {
-            transform.Translate(new Vector3(0, -1, 0));
+            transform.Translate(new Vector3(0, ChooseRandomTranslate(), 0));
         }
         else if (Input.GetKeyDown(KeyCode.A)) {
-            transform.Translate(new Vector3(-1, 0, 0));
+            transform.Translate(new Vector3(ChooseRandomTranslate(), 0, 0));
         }
         else if (Input.GetKeyDown(KeyCode.D)) {
-            transform.Translate(new Vector3(1, 0, 0));
+            transform.Translate(new Vector3(ChooseRandomTranslate(), 0, 0));
         }
+    }
+    
+    private int ChooseRandomTranslate() {
+        var translate = translateValue;
+        var rand = Random.Range(0, 100);
+
+        if (rand >= 50) {
+            translate = translateValue;
+        }
+        else {
+            translate = -translateValue;
+        }
+
+        return translate;
     }
 }
